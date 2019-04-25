6ddf1e058989017db16a70a20764a39d81652f9d
gangys0411 <gangys0411@gmail.com>
2019-04-25 10:43:07 +0900

    first

--- 201421246-sum.c
+++ 201421246-sum.c
@@ -1,2 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int main()
+{
+	int start=1;
+	int resert=0;
+	int num=10;
+	pid_t pid;	
+
+	pid=vfork();
+
+	for(int i=0; i<num; i++)
+	{
+		if(pid==0)
+		{
+			for(int j=0; j<100;j++)
+			{
+				resert=resert+start;
+				start++;
+			}
+			exit(0);
+		}
+
+		else if(pid>0)
+		{
+			pid=vfork();
+		}
+	}
+	printf("%d\n", resert);
+	return 0;
+}
